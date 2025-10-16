7e2d1cfa47f98b5bb308b09646965a37957122ce
dathellavader <ella.henner@pcc.edu>
2025-10-16 21:10:51 +0000

    almostdone

--- main.cpp
+++ main.cpp
@@ -68,7 +68,7 @@ else {
     cout << "No match." << endl;
 }
 
-cout << endl << "Thank you for using the Phrases program!" << endl;
+cout << endl << "Thank you for using the Phrases Program!" << endl;
 
 return 0;
 }
@@ -76,16 +76,19 @@ return 0;
 
 // ------------- DESIGN -------------
 /* 
-Program Name:
+Program Name: The Phrases Program
 
 Program Description:
 
 Design:
 A. INPUT
-Define the input variables including name data type. 
+string phrase1 = "";
+string phrase2 = "";
 
 B. OUTPUT
-Define the output variables including data types. 
+string phrase 1
+string phrase 2
+substr
 
 C. CALCULATIONS
 Describe calculations used by algorithms in step D.  
@@ -93,13 +96,55 @@ List all formulas.
 If there are no calculations needed, state there are no calculations.
 
 D. LOGIC and ALGORITHMS
-Design the logic of your program using pseudocode or flowcharts. 
-Use conditionals, loops, functions or array constructs.
-List the steps in transforming inputs into outputs. 
-https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
+
+MESSAGE "Welcome to the Phrases Program. Ready....Go!"
+
+PROMPT "Enter phrase one: "
+INPUT phrase1
+LABEL "You entered: "
+OUTPUT phrase1
+
+PROMPT "Enter phrase two: "
+INPUT phrase2
+LABEL "You entered: "
+OUTPUT phrase2
+
 
 
 SAMPLE RUNS
-Copy from assignment document.
+
+Welcome to the Phrases and Subphrases program!
+
+Enter Phrase 1: truck
+You entered: truck
+Enter Phrase 2: firetruck is here
+You entered: firetruck is here
+
+truck is found in firetruck is here
+truck is here
+
+Thank you for using my program!
+Welcome to the Phrases and Subphrases program!
+
+Enter Phrase 1: the green grass grows
+You entered: the green grass grows
+Enter Phrase 2: green grass
+You entered: green grass
+
+green grass is found in the green grass grows
+green grass grows
+
+Thank you for using my program!
+Welcome to the Phrases and Subphrases program!
+
+Enter Phrase 1: He was between a rock and a hard place
+You entered: He was between a rock and a hard place
+Enter Phrase 2: rock
+You entered: rock
+
+rock is found in He was between a rock and a hard place
+rock and a hard place
+
+Thank you for using my program!
 
 */
